6dad334b4c10ecbe631f226db226e483ff8c3d14
Deepikaik <deepikabhagavati2001@gmail.com>
2024-08-27 13:20:28 +0530

    'java'

---
+++ Basics/Test.java
@@ -0,0 +1,11 @@
+class Test{
+    public static void main(String[] args){
+        System.out.println("GA!");
+    }
+}
+class A{}
+class B{}
+class C{
+    public static void main(String[] args){
+        System.out.println("GE!");
+}}
